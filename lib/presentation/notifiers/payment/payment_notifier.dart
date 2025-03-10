ea1ab825fce48a281e93ddac63c1e2347aa3dd93
Umais Qureshi <umaisqureshi26@icloud.com>
2025-03-10 20:15:04 +0100

    Payment states

--- lib/presentation/notifiers/payment/payment_notifier.dart
+++ lib/presentation/notifiers/payment/payment_notifier.dart
@@ -6,21 +6,18 @@ class PaymentNotifier extends StateNotifier<PaymentState> {
       : _paymentRemoteService = paymentRemoteService,
         super(PaymentLoading());
 
- makePayment(int amount, String currency) async {
+  makePayment(int amount, String currency) async {
     try {
       await _paymentRemoteService.makePayment(amount, currency);
       await _paymentRemoteService.displayPaymentSheet().then((value) {
-        
         if (value) {
           state = PaymentSuccess();
         } else {
           state = PaymentError();
         }
       });
-
     } catch (e) {
       state = PaymentError();
-
     }
   }
 }
